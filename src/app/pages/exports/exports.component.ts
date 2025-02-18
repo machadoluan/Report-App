8c46a37057980e99e85eaab789dd72a4eeca8a70
machadoluan <machado.luandealmeida@gmail.com>
2025-02-18 11:56:33 -0300

    ✨ feat: add dashboard, exports, login, reports, and trips.

---
+++ src/app/pages/exports/exports.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exports',
+  imports: [],
+  templateUrl: './exports.component.html',
+  styleUrl: './exports.component.scss'
+})
+export class ExportsComponent {
+
+}
