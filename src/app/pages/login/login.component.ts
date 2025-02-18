942e2c4c3461722ce800fca2ad45ee3c80710bc9
machadoluan <machado.luandealmeida@gmail.com>
2025-02-18 13:53:12 -0300

    ✨ feat: enhance login component with improved UI and functionality

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Checkbox } from 'primeng/checkbox';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [Checkbox, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
+  showPassword = false;
+  rememberMe = false;
+
+  constructor() {}
+
+  toggleShowPassword() {
+    this.showPassword = !this.showPassword;
+  }
 }
