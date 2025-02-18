06ef45994e0dff1524ffcc5e2b4413dd98f185e8
machadoluan <machado.luandealmeida@gmail.com>
2025-02-18 16:53:05 -0300

    ✨ feat: implement conditional layout rendering with slidebar and header components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { SlidebarComponent } from './components/slidebar/slidebar.component';
+import { HeaderComponent } from "./components/header/header.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SlidebarComponent],
+  imports: [RouterOutlet, SlidebarComponent, HeaderComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'reportApp';
+
+  constructor(public router: Router) { }
+
+  isRouteLogin(): boolean {
+    return this.router.url === '/login';
+  }
 }
