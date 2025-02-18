f4c816f9d9b17e02a569f7aeac0169ecd209763d
machadoluan <machado.luandealmeida@gmail.com>
2025-02-18 15:34:23 -0300

    ✨ feat: enhance slidebar component with navigation links and user account section

--- src/app/components/slidebar/slidebar.component.ts
+++ src/app/components/slidebar/slidebar.component.ts
@@ -1,11 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-slidebar',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './slidebar.component.html',
   styleUrl: './slidebar.component.scss'
 })
 export class SlidebarComponent {
+
+  constructor(private router: Router){}
+
+  isActive(route: string): boolean {
+    return this.router.url === route;
+  }
 
 }
