b660dbcde7c6bf42c81f2d9b9d38cf34d2520c1a
machadoluan <machado.luandealmeida@gmail.com>
2025-02-18 15:34:02 -0300

    ✨ feat: integrate slidebar component into app layout with adjusted main content margin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SlidebarComponent } from './components/slidebar/slidebar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SlidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
