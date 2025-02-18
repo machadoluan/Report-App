0a83b1463b13d4d2ade430ff324aea7bc4f16eda
machadoluan <machado.luandealmeida@gmail.com>
2025-02-18 12:02:49 -0300

    ✨ feat: add routing for login, dashboard, reports, trips, and exports components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ReportsComponent } from './pages/reports/reports.component';
+import { TripsComponent } from './pages/trips/trips.component';
+import { ExportsComponent } from './pages/exports/exports.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'dashboard',
+        component: DashboardComponent
+    },
+    {
+        path: 'reports',
+        component: ReportsComponent
+    },
+    {
+        path: 'trips',
+        component: TripsComponent
+    },
+    {
+        path: 'exports',
+        component: ExportsComponent
+    }
+
+];
