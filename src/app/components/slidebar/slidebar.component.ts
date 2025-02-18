54a3366fd0c6dc426288fe85f9dbd328a15f0bed
machadoluan <machado.luandealmeida@gmail.com>
2025-02-18 14:34:18 -0300

    ✨ feat: add slidebar component

---
+++ src/app/components/slidebar/slidebar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-slidebar',
+  imports: [],
+  templateUrl: './slidebar.component.html',
+  styleUrl: './slidebar.component.scss'
+})
+export class SlidebarComponent {
+
+}
