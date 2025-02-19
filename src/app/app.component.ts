4406263a299bf73339e43aa5c287a5c2510d88e0
machadoluan <machado.luandealmeida@gmail.com>
2025-02-18 23:20:20 -0300

    ✨ feat: update app layout with responsive sidebar and add slidebar-mobile component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,37 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { SlidebarComponent } from './components/slidebar/slidebar.component';
 import { HeaderComponent } from "./components/header/header.component";
 import { CommonModule } from '@angular/common';
+import { SlidebarMobileComponent } from "./components/slidebar-mobile/slidebar-mobile.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SlidebarComponent, HeaderComponent, CommonModule],
+  imports: [RouterOutlet, SlidebarComponent, HeaderComponent, CommonModule, SlidebarMobileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   isSidebarOpen = false;
   title = 'reportApp';
 
   constructor(public router: Router) { }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.checkScreenWidth();
+  }
+
+  checkScreenWidth() {
+    if (window.innerWidth <= 990) {
+      this.isSidebarOpen = false;
+    }
+    else {
+      this.isSidebarOpen = true;
+    }
+  }
+
 
   isRouteLogin(): boolean {
     return this.router.url === '/login';
