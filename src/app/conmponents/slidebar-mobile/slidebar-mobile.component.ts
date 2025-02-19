7c291d1ae622a762d755db201b784024ab6a5334
machadoluan <machado.luandealmeida@gmail.com>
2025-02-18 21:49:27 -0300

    ✨ feat: add slidebar-mobile component with basic structure and template

---
+++ src/app/conmponents/slidebar-mobile/slidebar-mobile.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-slidebar-mobile',
+  imports: [],
+  templateUrl: './slidebar-mobile.component.html',
+  styleUrl: './slidebar-mobile.component.scss'
+})
+export class SlidebarMobileComponent {
+
+}
