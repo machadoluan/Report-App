63bee2bd240c1e023181eb97b7c0889d9e448cd6
machadoluan <machado.luandealmeida@gmail.com>
2025-02-18 23:21:25 -0300

    ✨ feat: implement slidebar-mobile component with navigation links and responsive styling

--- src/app/components/slidebar-mobile/slidebar-mobile.component.ts
+++ src/app/components/slidebar-mobile/slidebar-mobile.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-slidebar-mobile',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './slidebar-mobile.component.html',
   styleUrl: './slidebar-mobile.component.scss'
 })
 export class SlidebarMobileComponent {
-
+   
 }
