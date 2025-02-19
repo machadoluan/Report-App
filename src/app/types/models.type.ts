4f66a675826725cfb12f2e8b491a7e895ab93d0a
machadoluan <machado.luandealmeida@gmail.com>
2025-02-18 21:50:23 -0300

    ✨ feat: enhance viagem and registro types with additional fields for improved data management

--- src/app/types/models.type.ts
+++ src/app/types/models.type.ts
@@ -1,16 +1,19 @@
 export type viagem = {
-    origem: string,
-    destino: string, 
-    data_inicio: string,
-    data_fim: string,
-    status: string,
-    id: number
+    origem: string;
+    cliente: string;
+    destino: string;
+    data_inicio: string;
+    data_fim: string;
+    status: string;
+    valor: number;
+    id: number;
 }
 
 export type registro = {
-    viagem_id: number,  
-    tipo: string,
-    data: string,
-    hora: string,
-    descricao: string
+    viagem_id: number;
+    tipo: string;
+    data: string;
+    hora: string;
+    descricao: string;
+    // img: string;
 }
