4593f13d34916e57ef228990e54291a8645f707b
machadoluan <machado.luandealmeida@gmail.com>
2025-02-20 00:04:42 -0300

    feat: create reports component with initial template and styles

---
+++ src/app/components/create-reports/create-reports.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-reports',
+  imports: [],
+  templateUrl: './create-reports.component.html',
+  styleUrl: './create-reports.component.scss'
+})
+export class CreateReportsComponent {
+
+}
