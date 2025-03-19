2a993dadcdc0be041e654a74132a6d704ff675de
machadoluan <machado.luandealmeida@gmail.com>
2025-03-19 19:45:07 -0300

    feat: tags meta

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:3000', // URL da API no backend
+    apiUrl: 'http://132.255.58.65:1351', // URL da API no backend
   };
   
