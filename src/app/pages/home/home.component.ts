375d1979a2dd7704465f27bca7a0879d2b832f79
machadoluan <machado.luandealmeida@gmail.com>
2025-04-26 23:31:06 -0300

    feat: new desing mobile

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { CarouselModule } from 'primeng/carousel'
 
 @Component({
   selector: 'app-home',
-  imports: [CarouselModule, ButtonModule],
+  imports: [CarouselModule, ButtonModule, RouterLink, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent implements OnInit {
+  menuMobile: boolean = false
   ngOnInit(): void {
 
+  }
+
+
+  toggleMenuBar() {
+    this.menuMobile = !this.menuMobile
   }
 }
 
